0960f667981a1ded8dd94922ffd3bfb21e6613f8
leolace <lgonsalez02@gmail.com>
2025-05-25 13:20:29 -0300

    feat: add multiples directions to Caveira

--- src/Modelo/Caveira.java
+++ src/Modelo/Caveira.java
@@ -2,15 +2,19 @@ package Modelo;
 
 import Auxiliar.Consts;
 import Controler.GameControl;
+import java.util.Random;
 
 public class Caveira extends Entity {
   private int iContaIntervalos;
+  private Random random;
+  
   public Caveira(String filename) {
     super(filename);
     this.isTransposable = true;
     this.isMortal = false; // Causa dano ao herói
     this.isDangerous = true; // Causa dano ao herói
     this.iContaIntervalos = 0;
+    this.random = new Random();
   }
 
   @Override
@@ -18,9 +22,42 @@ public class Caveira extends Entity {
     this.iContaIntervalos++;
     if (this.iContaIntervalos == Consts.TIMER) {
       this.iContaIntervalos = 0;
-      Fogo f = new Fogo("fire.png");
-      f.setPosicao(position.getLinha(), position.getColuna() + 1);
+      
+      // Escolhe uma direção aleatória
+      Fogo.Direction randomDirection = getRandomDirection();
+      
+      // Cria o fogo com direção aleatória
+      Fogo f = new Fogo("fire.png", randomDirection);
+      
+      // Posiciona o fogo baseado na direção
+      setPosicaoBasedOnDirection(f, randomDirection);
+      
+      // Adiciona o fogo ao nível atual
       GameControl.getCurrentLevel().addPersonagem(f);
+    }
+  }
+  
+  // Método para escolher uma direção aleatória
+  private Fogo.Direction getRandomDirection() {
+    Fogo.Direction[] directions = Fogo.Direction.values();
+    return directions[random.nextInt(directions.length)];
+  }
+  
+  // Método para posicionar o fogo baseado na direção
+  private void setPosicaoBasedOnDirection(Fogo fogo, Fogo.Direction direction) {
+    switch (direction) {
+      case UP:
+        fogo.setPosicao(position.getLinha() - 1, position.getColuna());
+        break;
+      case RIGHT:
+        fogo.setPosicao(position.getLinha(), position.getColuna() + 1);
+        break;
+      case DOWN:
+        fogo.setPosicao(position.getLinha() + 1, position.getColuna());
+        break;
+      case LEFT:
+        fogo.setPosicao(position.getLinha(), position.getColuna() - 1);
+        break;
     }
   }
 }
--- src/Modelo/Fogo.java
+++ src/Modelo/Fogo.java
@@ -3,18 +3,51 @@ package Modelo;
 import Controler.GameControl;
 
 public class Fogo extends Entity {
+  // Enumeração para as direções possíveis do fogo
+  public enum Direction {
+    UP, RIGHT, DOWN, LEFT
+  }
+  
+  private Direction direction;
 
   public Fogo(String filename) {
     super(filename);
     this.isMortal = true;
     this.isTransposable = true;
+    this.isDangerous = true;
     this.setMovementDelay(3);
+    this.direction = Direction.RIGHT; // Direção padrão é para direita
+  }
+  
+  public Fogo(String filename, Direction direction) {
+    this(filename);
+    this.direction = direction;
   }
 
   @Override
   protected void movement() {
-    if (!this.moveRight())
+    boolean movementSuccess = false;
+    
+    // Move na direção especificada
+    switch (this.direction) {
+      case UP:
+        movementSuccess = this.moveUp();
+        break;
+      case RIGHT:
+        movementSuccess = this.moveRight();
+        break;
+      case DOWN:
+        movementSuccess = this.moveDown();
+        break;
+      case LEFT:
+        movementSuccess = this.moveLeft();
+        break;
+    }
+    
+    // Se não conseguir mover, remove o fogo
+    if (!movementSuccess) {
       GameControl.getCurrentLevel().removePersonagem(this);
+    }
   }
 
 }
